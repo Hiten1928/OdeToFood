ccf7c1f9456fe91d1891af289032e84c69f9f5ac
Victoria Besedina <victoria.besedina@gmail.com>
2015-08-20 12:07:22 +0300

    table booking index view add

---
+++ OdeToFood/Controllers/BookTableController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class BookTableController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+//        public ActionResult PlaceOrder()
+//        {
+//            
+//        }
+    }
+}
