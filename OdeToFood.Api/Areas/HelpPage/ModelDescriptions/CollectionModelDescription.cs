686d103bbde8fd5e7cd29769d1c69adb98929351
Victoria Besedina <victoria.besedina@gmail.com>
2015-09-02 15:43:57 +0300

    Delete web api in separate project

--- OdeToFood.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace OdeToFood.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- OdeToFood.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace OdeToFood.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
